5d41df819ed18e16da0c79f1805f065b392ab99e
uzdevid <uzdevid@gmail.com>
2025-04-18 14:39:29 +0500

    cleanup

--- tests/Support/config/centrifugo/di/handlers.php
+++ tests/Support/config/centrifugo/di/handlers.php
@@ -1,6 +1,5 @@
 <?php declare(strict_types=1);
 
-
 use UzDevid\Yii\Runner\Centrifugo\Handler\ConnectHandlerInterface;
 use UzDevid\Yii\Runner\Centrifugo\Handler\InvalidRequestHandlerInterface;
 use UzDevid\Yii\Runner\Centrifugo\Handler\PublishHandlerInterface;
